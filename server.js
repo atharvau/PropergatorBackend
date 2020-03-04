776498933f0ec2820423f7726d683cdf55ce8032
athrva udapure <avu.ceo@gmail.com>
2020-03-04 20:07:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.htm"));
+});
+
+app.get("/video", function(req, res) {
+  var age = req.query.age + "";
+  var path = "assets/sample.mp4";
+
+  if (age === "1") {
+    path = "assets/kids.mp4";
+  } else if (age === "2") {
+    path = "assets/mc.mp4";
+  } else if (age === "3") {
+    path = "assets/pay.mp4";
+  } else if (age === "4") {
+    path = "assets/travel.mp4";
+  }
+
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    if (start >= fileSize) {
+      res
+        .status(416)
+        .send("Requested range not satisfiable\n" + start + " >= " + fileSize);
+      return;
+    }
+
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4"
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000!");
+});
